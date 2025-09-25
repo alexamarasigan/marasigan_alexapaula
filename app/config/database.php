b6a8151f55421956809930898f89c31a615504fd
alexamarasigan <marasiganalexa04@gmail.com>
2025-09-25 09:48:08 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'marasigan_alexapaula',
+    'username'	=> 'sql12800015',
+    'password'	=> 'tnXvTfZpRD',
+    'database'	=> 'sql12800015',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
